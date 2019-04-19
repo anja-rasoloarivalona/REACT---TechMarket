e8209cdf79a409117f16d6efc7b62745fe71c739
Anja <rasoloanja@gmail.com>
2019-04-19 15:52:17 -0400

    Init product image

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const storeProducts = [
     {
         id: 1,
         title: "product 1",
-        img: "img/product",
+        img: "./assets/img/product/product.png",
         price: 10,
         brand: "Nike",
         info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
@@ -14,7 +14,7 @@ export const storeProducts = [
     {
         id: 2,
         title: "product 2",
-        img: "img/product",
+        img: "./assets/img/product/product.png",
         price: 10,
         brand: "Nike",
         info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
@@ -26,7 +26,7 @@ export const storeProducts = [
     {
         id: 3,
         title: "product 3",
-        img: "img/product",
+        img: "./assets/img/product/product.png",
         price: 10,
         brand: "Nike",
         info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
@@ -38,7 +38,7 @@ export const storeProducts = [
     {
         id: 4,
         title: "product 4",
-        img: "img/product",
+        img: "./assets/img/product/product.png",
         price: 10,
         brand: "Nike",
         info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
@@ -50,7 +50,7 @@ export const storeProducts = [
     {
         id: 5,
         title: "product 5",
-        img: "img/product",
+        img: "./assets/img/product/product.png",
         price: 10,
         brand: "Nike",
         info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
@@ -62,7 +62,7 @@ export const storeProducts = [
     {
         id: 6,
         title: "product 6",
-        img: "img/product",
+        img: "./assets/img/product/product.png",
         price: 10,
         brand: "Nike",
         info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
@@ -74,7 +74,7 @@ export const storeProducts = [
     {
         id: 7,
         title: "product 7",
-        img: "img/product",
+        img: "./assets/img/product/product.png",
         price: 10,
         brand: "Nike",
         info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
@@ -86,7 +86,7 @@ export const storeProducts = [
     {
         id: 8,
         title: "product 8",
-        img: "img/product",
+        img: "./assets/img/product/product.png",
         price: 10,
         brand: "Nike",
         info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
@@ -98,14 +98,28 @@ export const storeProducts = [
     {
         id: 9,
         title: "product 9",
-        img: "img/product",
+        img: "./assets/img/product/product.png",
         price: 10,
         brand: "Nike",
         info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
         inCart: false,
         count: 0,
         total: 0
-    },
+    }
+
+];
+
+export const detailProduct = {
+    id: 1,
+    title: "product 1",
+    img: "./assets/img/product/product.png",
+    price: 10,
+    brand: "nike",
+    info:
+      "Lorem ipsum dolor amet offal butcher quinoa sustainable gastropub, echo park actually green juice sriracha paleo. Brooklyn sriracha semiotics, DIY coloring book mixtape craft beer sartorial hella blue bottle. Tote bag wolf authentic try-hard put a bird on it mumblecore. Unicorn lumbersexual master cleanse blog hella VHS, vaporware sartorial church-key cardigan single-origin coffee lo-fi organic asymmetrical. Taxidermy semiotics celiac stumptown scenester normcore, ethical helvetica photo booth gentrify.",
+    inCart: false,
+    count: 0,
+    total: 0
+  };
 
 
-]
