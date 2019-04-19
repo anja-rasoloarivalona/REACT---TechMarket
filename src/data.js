110022d0858f75559716f3994a31d0b8bdbf5d29
Anja <rasoloanja@gmail.com>
2019-04-19 15:46:03 -0400

    init data

---
+++ src/data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+    {
+        id: 1,
+        title: "product 1",
+        img: "img/product",
+        price: 10,
+        brand: "Nike",
+        info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 2,
+        title: "product 2",
+        img: "img/product",
+        price: 10,
+        brand: "Nike",
+        info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 3,
+        title: "product 3",
+        img: "img/product",
+        price: 10,
+        brand: "Nike",
+        info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 4,
+        title: "product 4",
+        img: "img/product",
+        price: 10,
+        brand: "Nike",
+        info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 5,
+        title: "product 5",
+        img: "img/product",
+        price: 10,
+        brand: "Nike",
+        info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 6,
+        title: "product 6",
+        img: "img/product",
+        price: 10,
+        brand: "Nike",
+        info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 7,
+        title: "product 7",
+        img: "img/product",
+        price: 10,
+        brand: "Nike",
+        info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 8,
+        title: "product 8",
+        img: "img/product",
+        price: 10,
+        brand: "Nike",
+        info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+    {
+        id: 9,
+        title: "product 9",
+        img: "img/product",
+        price: 10,
+        brand: "Nike",
+        info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime quibusdam animi perspiciatis repellendus ipsam recusandae fugit facilis debitis quos fuga!",
+        inCart: false,
+        count: 0,
+        total: 0
+    },
+
+
+]
