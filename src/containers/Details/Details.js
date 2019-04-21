97690f27cfb35f15f862e5c819900deeb96dc648
Anja <rasoloanja@gmail.com>
2019-04-21 17:47:24 -0400

    Added button in details section

--- src/containers/Details/Details.js
+++ src/containers/Details/Details.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {ProductConsumer} from '../../context';
 import {Link} from 'react-router-dom';
 import classes from './Details.css';
+import ButtonContainer from '../../components/Buttons/Button';
 
 class Details extends Component {
   render() {
@@ -11,24 +12,36 @@ class Details extends Component {
           const { brand, img, info, price, subtitle, inCart} = value.productsDetail;
 
           return (
-            <div className={classes.Details}>
+            <div className={classes.Container}>
                 <div className={classes.ImgContainer}>
                   <img className={classes.Img} src={img}/>
                 </div>
          
-                <div className={classes.Info}>
-                    <h1> {brand}</h1>
-                    <h1> {subtitle}</h1>
+                <div className={classes.Details}>
+                    <h1 className={classes.Title}> {brand}</h1>
+                    <h1 className={classes.Title}> {subtitle}</h1>
                     
-                    <h4>
+                    <h4 className={classes.Price}>
                       price: ${price}
                     </h4>
-                    <p className={classes.InfoContainer}> 
+
+                    <div className={classes.InfoContainer}> 
                       Some info about the product
-                    </p>
-                    <p>
-                      {info}
-                    </p>
+                        <p className={classes.Info}>
+                          {info}
+                        </p>
+                    </div>
+                    
+                    <div className={classes.Cta}>
+                        <Link to='/' className={classes.Link}>
+                            <ButtonContainer className={classes.Button}>
+                              Back
+                            </ButtonContainer>
+                        </Link>
+                        <ButtonContainer className={classes.Button}>
+                            Add to cart
+                        </ButtonContainer>
+                    </div>
                 </div>
             </div>
           )
