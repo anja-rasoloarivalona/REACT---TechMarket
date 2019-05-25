60fe9c9f05cdaa605ae91d810a82719fd03c023c
Anja <rasoloanja@gmail.com>
2019-05-24 22:25:31 -0400

    Init Footer Item

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,24 @@ import classes from './Footer.css';
 const footer = ()=>  {
   return (
     <div className={classes.Footer}>
-      Hello from footer
+
+
+      <div className={classes.FooterItem}>
+          <div className={classes.TitleContainer}>
+              <div className={classes.Title}>
+                  <span>T</span>
+                  <span>ech</span>
+              </div>
+              <div className={classes.Title}>
+                  <span>M</span>
+                  <span>arket</span>
+              </div>
+          </div>
+          <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia.</p>
+          <div className={classes.SocialMedia}></div>
+      </div>
+
+
     </div>
   )
 }
