9e679080252449481e3546fb2219cc4885917131
Anja <rasoloanja@gmail.com>
2019-05-30 14:35:00 -0400

    Added search logic to cart component

--- src/containers/Cart/Cart.js
+++ src/containers/Cart/Cart.js
@@ -8,7 +8,8 @@ import ReactSVG from 'react-svg';
 import cartIcon from '../../assets/icon/SVG/cart.svg';
 import { Link } from 'react-router-dom';
 
-
+import ProductNotFound from '../Products/ProductNotFound/ProductNotFound';
+import Product from '../../components/Product/Product';
 
 
  class Cart extends Component {
@@ -22,36 +23,103 @@ import { Link } from 'react-router-dom';
       <section className={classes.Cart}>
         <ProductConsumer>
             {value => {
-              const {cart} = value;
+              const {cart, searchedValue, products} = value;
+              let searchedProducts = [...products];
 
-              if(cart.length > 0) {
+              if (searchedValue !== "") {         
+                searchedProducts = searchedProducts.filter(item => {
 
-                return (
-                  <React.Fragment>
-                    <CartItems value={value}/>
-                    <CartTotals value={value} history={this.props.history} />
-                  </React.Fragment>
-                );
+                    if( item.brand
+                        .toLowerCase()
+                        .search(searchedValue.toLowerCase()) !== -1) {
+                    return item
 
-              } else {
-                return (
+                } else {
 
-                  <div className={classes.EmptyCart}>
-                      <ReactSVG src={cartIcon} alt="Icon" className={classes.Icon}/>
-                      <div className={classes.Title}>Your cart is currently empty</div>
-                      <div className={classes.Text}>
-                        <div>Before proceed to checkout you must add some products to your shopping cart</div>
-                        <div>You will find a lot of interesting prodcuts on our "Store" page.</div>
-                      </div>
+                }  if( item.type
+                    .toLowerCase()
+                    .search(searchedValue.toLowerCase()) !== -1) {
+                        return item     
+                    } else {
+                        if( item.model
+                            .toLowerCase()
+                            .search(searchedValue.toLowerCase()) !== -1) {
+                                return item     
+                            } else {
+                                if( item.color
+                                    .toLowerCase()
+                                    .search(searchedValue.toLowerCase()) !== -1) {
+                                        return item     
+                                    }
+                            }
+                    }
+            
+                })
+
+
+
+                if(searchedProducts.length > 0) {
+                        return (<div className={classes.List}>
+                              {
+                                          searchedProducts.map(item => {
+                                            return (
+                                                <Product 
+                                                        key={item.id}
+                                                        product={item}/>
+                                            
+                                            )      
+                                          })
+                              }
+                              </div>)
                       
-                      
-                      <Link to="/store" className={classes.Link}>
-                          Return to store
-                      </Link>
-                  </div>
-                )
-                  
+
+                   } else {
+                       return  (
+
+                        <div className={classes.List}>
+                              <ProductNotFound />
+                        </div>
+                       )
+                        
+                   }
+                
+            
+                
+                } else {
+
+                if(cart.length > 0) {
+
+                  return (
+                    <React.Fragment>
+                      <CartItems value={value}/>
+                      <CartTotals value={value} history={this.props.history} />
+                    </React.Fragment>
+                  );
+  
+                } else {
+                  return (
+  
+                    <div className={classes.EmptyCart}>
+                        <ReactSVG src={cartIcon} alt="Icon" className={classes.Icon}/>
+                        <div className={classes.Title}>Your cart is currently empty</div>
+                        <div className={classes.Text}>
+                          <div>Before proceed to checkout you must add some products to your shopping cart</div>
+                          <div>You will find a lot of interesting prodcuts on our "Store" page.</div>
+                        </div>
+                        
+                        
+                        <Link to="/store" className={classes.Link}>
+                            Return to store
+                        </Link>
+                    </div>
+                  )
+                    
+                }
+
               }
+
+
+              
 
 
             }}
